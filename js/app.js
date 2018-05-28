4e8c98f842fcd0b257eb534199cadc3b80ed6aee
Jason Bodman <bodmand1@gmail.com>
2018-05-27 23:04:14 -0400

    remove unnecessary code and update comments

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var Location;
 var clientID;
 var clientSecret;
 
-// create a global variable for last marker
+// Create a global variable for last marker
 var lastClickedMarker = null;
 var openedInfoWindow = null;
 
@@ -130,98 +130,92 @@ Location = function(data) {
   this.city = '';
   this.phone = '';
 
-  // By default every marker will be visible
+  // Make sure all markers are visible by default
   this.visible = ko.observable(true);
 
-	// Foursquare API settings
+	// Set Foursquare API details
 	clientID = "U2HV0YOLPT2X5TDODTGJW4YFY4DB0SEXIUM1WI2VSFXK2C3M";
 	clientSecret = "0X5PTPK5X3RVG3JE5T3SMC5W53X2V22KC4RZYZRPG2TRFXO2";
 
-	// Foursquare API Link to call.
+	// Set Foursquare API link
 	 var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll=' + this.lat + ',' + this.long + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20170413' + '&query=' + this.name;
 
-	 // Gets the data from foursquare and store it into its' own variables.
-	 $.getJSON(foursquareURL).done(function (data) {
-			 var results = data.response.venues[0];
-			 self.URL = results.url;
-			 if (typeof self.URL === 'undefined') {
-					 self.URL = "";
-			 }
-			 self.street = results.location.formattedAddress[0] || 'No Address Provided';
-			 self.city = results.location.formattedAddress[1] || 'No Address Provided';
-			 self.phone = results.contact.formattedPhone || 'No Phone Provided';
-	 }).fail(function () {
-			 $('.list').html('There was an error with the Foursquare API call. Please refresh the page and try again to load Foursquare data.');
-	 });
+	// Stores Foursquare data as its own variable
+	$.getJSON(foursquareURL).done(function (data) {
+		var results = data.response.venues[0];
+		self.URL = results.url;
+		if (typeof self.URL === 'undefined') {
+				self.URL = "";
+		}
+		self.street = results.location.formattedAddress[0] || 'No Address Provided';
+		self.city = results.location.formattedAddress[1] || 'No Address Provided';
+		self.phone = results.contact.formattedPhone || 'No Phone Provided';
+	}).fail(function () {
+		$('.list').html('There was an error with the Foursquare API call. Please refresh the page and try again to load Foursquare data.');
+	});
 
-	 // This is what the infowindow will contain when clicked.
-	 this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
-			 '<div class="content"><a href="' + self.URL + '">' + self.URL + "</a></div>" +
-			 '<div class="content">' + self.street + "</div>" +
-			 '<div class="content">' + self.city + "</div>" +
-			 '<div class="content">' + self.phone + "</div></div>"+
-			 '<br><div class="content">Information provided by <a href="http://www.foursquare.com">Foursquare</a>.</div></div>';
+	// Set infoWindow content
+	this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+		'<div class="content"><a href="' + self.URL + '">' + self.URL + "</a></div>" +
+		'<div class="content">' + self.street + "</div>" +
+		'<div class="content">' + self.city + "</div>" +
+		'<div class="content">' + self.phone + "</div></div>"+
+		'<br><div class="content">Information provided by <a href="http://www.foursquare.com">Foursquare</a>.</div></div>';
 
-	 // Puts the content string inside infowindow.
-	 this.infoWindow = new google.maps.InfoWindow({content: self.contentString});
+	// Place contentString inside of InfoWindow
+	this.infoWindow = new google.maps.InfoWindow({content: self.contentString});
 
-	 // Places the marker to it's designed location on the map along with it's title.
-	 this.marker = new google.maps.Marker({
-			 position: new google.maps.LatLng(data.lat, data.long),
-			 map: map,
-			 title: data.name
-	 });
+	// Sets marker on map
+	this.marker = new google.maps.Marker({
+		position: new google.maps.LatLng(data.lat, data.long),
+		map: map,
+		title: data.name
+	});
 
-	 // Only makes the one selected marker visible.
-	 this.showMarker = ko.computed(function() {
-			 if(this.visible() === true) {
-					 this.marker.setMap(map);
-			 } else {
-					 this.marker.setMap(null);
-			 }
-			 return true;
-	 }, this);
+	// Open marker when clicked and close previously opened markers
+	this.marker.addListener('click', function(){
+		// Close previously opened infoWindow
+		if (openedInfoWindow) {
+			openedInfoWindow.close();
+		}
 
-	 // When marker is clicked on open up infowindow designated to the marker with it's information.
-	 this.marker.addListener('click', function(){
-			 // close opened infoWindow
-			 if (openedInfoWindow) {
-				 openedInfoWindow.close();
-			 }
+		// Set clicked marker as lastClickedMarker
+		var cancelAnimation = function() {
+			lastClickedMarker.setAnimation(null);
+			lastClickedMarker = null;
+		};
 
-			 var cancelAnimation = function() {
-				 lastClickedMarker.setAnimation(null);
-				 lastClickedMarker = null;
-			 };
+		//
+		if (lastClickedMarker) {
+			cancelAnimation();
+		}
 
-			 if (lastClickedMarker) {
-				 cancelAnimation();
-			 }
+		// Set contentString with clicked marker info
+		self.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+				'<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
+				'<div class="content">' + self.street + "</div>" +
+				'<div class="content">' + self.city + "</div>" +
+				'<div class="content"><a href="tel:' + self.phone +'">' + self.phone +"</a></div>" +
+				'<br><div class="content">Information provided by <a href="http://www.foursquare.com">Foursquare</a>.</div></div>';
 
-			 self.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
-					 '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
-					 '<div class="content">' + self.street + "</div>" +
-					 '<div class="content">' + self.city + "</div>" +
-					 '<div class="content"><a href="tel:' + self.phone +'">' + self.phone +"</a></div>" +
-					 '<br><div class="content">Information provided by <a href="http://www.foursquare.com">Foursquare</a>.</div></div>';
+		self.infoWindow.setContent(self.contentString);
+		openedInfoWindow = self.infoWindow;
 
-			 self.infoWindow.setContent(self.contentString);
-			 openedInfoWindow = self.infoWindow;
+		// Set selected marker on map and animate
+		self.infoWindow.open(map, self.marker);
+		self.marker.setAnimation(google.maps.Animation.BOUNCE);
+		setTimeout(function() {
+				self.marker.setAnimation(null);
+		}, 2100);
+		lastClickedMarker = self.marker;
 
-			 self.infoWindow.open(map, self.marker);
-			 self.marker.setAnimation(google.maps.Animation.BOUNCE);
-			 setTimeout(function() {
-					 self.marker.setAnimation(null);
-			 }, 2100);
-			 lastClickedMarker = self.marker;
+		google.maps.event.addListener(infoWindow, 'closeclick', cancelAnimation);
+	});
 
-			 google.maps.event.addListener(infoWindow, 'closeclick', cancelAnimation);
-	 });
-
-	 // Makes the marker bounce animation whenever clicked.
-	 this.bounce = function(place) {
-			 google.maps.event.trigger(self.marker, 'click');
-	 };
+	// Makes the marker bounce animation whenever clicked.
+	this.bounce = function(place) {
+		google.maps.event.trigger(self.marker, 'click');
+	};
 };
 
 
@@ -230,69 +224,57 @@ Google Maps API
 *******************************/
 function ViewModel(){
 
-	 var self = this;
+	var self = this;
 
-	 //Holds value for list togglings
-	 this.toggleSymbol = ko.observable('hide');
+	// Holds value for list togglings
+	this.toggleSymbol = ko.observable('hide');
 
-	 // Search term is blank by default
-	 this.searchTerm = ko.observable('');
+	// Set search term to be blank by default
+	this.searchTerm = ko.observable('');
 
-	 // Initializes a blank array for locations
-	 this.locationList = ko.observableArray([]);
+	// Sets a blank array for locations
+	this.locationList = ko.observableArray([]);
 
-	 // Constructor creates a new map - only center and zoom are required.
-	 map = new google.maps.Map(document.getElementById('map'), {
-     center: {lat: 39.9524, lng: -75.1636},
-     zoom: 11,
- 		 mapTypeControl: false,
-		 styles: styles
-   });
+	// Create new map centered on Philadelphia's City Hall
+	map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 39.9524, lng: -75.1636},
+    zoom: 11,
+ 		mapTypeControl: false,
+		styles: styles
+  });
 
-	 // Centers map when compass is clicked on.
-	 this.centerMap = function(){
-			 map.setCenter({lat: 39.9524, lng: -75.1636});
-	 };
+	// Pushes default locations array into new location list array
+	defaultLocations.forEach(function(locationItem){
+			self.locationList.push( new Location(locationItem));
+	});
 
-	 //toggles the list view
-	 this.listToggle = function() {
-			 if(self.toggleSymbol() === 'hide') {
-					 self.toggleSymbol('show');
-			 } else {
-					 self.toggleSymbol('hide');
-			 }
-	 };
-
-	 // Pushes default locations array into new location list array
-	 defaultLocations.forEach(function(locationItem){
-			 self.locationList.push( new Location(locationItem));
-	 });
-
-	 // Searches for what user typed in the input bar using the locationlist array.
-	 // Only displaying the exact item results that user type if available in the locationlist array.
-	 this.filteredList = ko.computed( function() {
-			 var filter = self.searchTerm().toLowerCase();
-			 if (!filter) {
-					 self.locationList().forEach(function(locationItem){
-							 locationItem.visible(true);
-					 });
-					 return self.locationList();
-			 } else {
-					 return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
-							 var string = locationItem.name.toLowerCase();
-							 var result = (string.search(filter) >= 0);
-							 locationItem.visible(result);
-							 return result;
-					 });
-			 }
-	 }, self);
+	// Searches for user input from search field using the locationlist array
+	// Search will only display an exact match if available in the locationlist array
+	this.filteredList = ko.computed( function() {
+			var filter = self.searchTerm().toLowerCase();
+			if (!filter) {
+					self.locationList().forEach(function(locationItem){
+							locationItem.visible(true);
+					});
+					return self.locationList();
+			} else {
+					return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
+							var string = locationItem.name.toLowerCase();
+							var result = (string.search(filter) >= 0);
+							locationItem.visible(result);
+							return result;
+					});
+			}
+	}, self);
 }
 
 // Error handling if map doesn't load.
 function errorHandlingMap() {
-	 $('#map').html('We had trouble loading Google Maps. Please refresh your browser and try again.');
+	$('#map').html('We had trouble connecting to Google Maps. Please refresh your browser and try again in a minute.');
 }
 
+
+// Function that starts process of loading map and locations
 function startApp() {
-	 ko.applyBindings(new ViewModel());
+	ko.applyBindings(new ViewModel());
 }
